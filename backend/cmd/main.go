64c84a603dd11a2d9a28660094aeb31e182ee871
arthurshafikov <arthur.shafikov27@gmail.com>
2022-08-30 07:13:15 +0400

    init

---
+++ backend/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/arthurshafikov/appcreative/backend/internal/app"
+
+func main() {
+	app.Run()
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/arthurshafikov/appcreative/backend
+
+go 1.18
