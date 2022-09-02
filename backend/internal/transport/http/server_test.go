7b4b5b20aee1e5aff5d2ec26bae81a1481610e84
arthurshafikov <arthur.shafikov27@gmail.com>
2022-09-02 07:40:36 +0400

    add server unit test

---
+++ backend/internal/transport/http/server_test.go
@@ -0,0 +1,36 @@
+package http
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	mock_server "github.com/arthurshafikov/appcreative/backend/internal/transport/http/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
+)
+
+func TestServe(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	handlerMock := mock_server.NewMockHandler(ctrl)
+	loggerMock := mock_server.NewMockLogger(ctrl)
+	server := NewServer(handlerMock, loggerMock)
+	ctx, cancel := context.WithCancel(context.Background())
+	group, ctx := errgroup.WithContext(ctx)
+	gomock.InOrder(
+		handlerMock.EXPECT().InitRoutes(gomock.Any()),
+		loggerMock.EXPECT().Info("Shutdown Server ..."),
+	)
+	group.Go(func() error {
+		time.Sleep(time.Second / 100)
+		cancel()
+
+		return nil
+	})
+
+	server.Serve(group, ctx, "9999")
+
+	require.Equal(t, ":9999", server.httpSrv.Addr)
+	require.NoError(t, group.Wait())
+}
